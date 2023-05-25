7cd6b96cc5a949e8736cb8ef57db1b199999ed2f
milan stojanov <stojanovmilan15@gmail.com>
2023-05-25 16:15:44 +0200

    tictactoe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { GameService } from './service/game.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Tic-Tac-Toe';
+  constructor (public gameService: GameService) {
+
+  }
+  resetGame() {
+    this.gameService.newGame();
+  }
 }
